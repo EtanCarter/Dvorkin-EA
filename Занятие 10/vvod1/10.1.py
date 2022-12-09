3feb09333afe122ce0dd8d2090aa415397c0263d
Evgeniy <112860096+EtanCarter@users.noreply.github.com>
2022-12-09 13:43:46 +0300

    Update

--- Занятие 10/vvod1/10.1.py
+++ Занятие 10/vvod1/10.1.py
@@ -1,15 +1,14 @@
 #-*- coding: utf-8 -*-
 file = open(r"C:\Users\EthanCarter\Desktop\Dvorkin.E.A_Y-224_vvod1.txt",encoding='utf-8')
-v = open(r"C:\Users\EthanCarter\Desktop\Dvorkin.E.A_Y-224_vvod1.txt",encoding='utf-8')
-vv = open(r"C:\Users\EthanCarter\Desktop\Dvorkin.E.A_Y-224_vivod1.txt", 'w+',encoding='utf-8')
-T = v.readlines()
-O = file.readlines(5)
-N = file.readlines()
+file2 = open(r"C:\Users\EthanCarter\Desktop\Dvorkin.E.A_Y-224_vivod1.txt", 'w+',encoding='utf-8')
+T = file.readlines()
+O = file2.readlines(5)
+N = file2.readlines()
 a = [int(x) for x in T]
 b = [int(x) for x in O]
 z = [int(x) for x in N]
 def F(c,d):
-    vv.write('number:')
+    file2.write('number:')
     for i in b:
         if i == c:
             vv.write('0')
@@ -18,13 +17,13 @@ def F(c,d):
         if i == c:
             vv.write('1')
             break
-    vv.write('\n')
+    file2.write('\n')
     for i in a:
         if i == c:
             i *= d
-            vv.write(str(i))
+            file2.write(str(i))
         else:
-            vv.write(str(i))
-    vv.write('\n')
+            file2.write(str(i))
+    file2.write('\n')
 F(2,3)
-vv.close() 
+file2.close() 
